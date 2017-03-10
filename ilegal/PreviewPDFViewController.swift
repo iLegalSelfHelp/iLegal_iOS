a19d49f0f0f97a6f699048e0ad4ec032dc981958
Matthew Rigdon <mrigdon@Matthews-MacBook-Pro-3.local>
2017-03-09 16:56:46 -0800

    removed image in categories vc and switched back to scroll view instead of prog bar

--- ilegal/PreviewPDFViewController.swift
+++ ilegal/PreviewPDFViewController.swift
@@ -17,33 +17,27 @@ class PreviewPDFViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        pdfWebView.loadRequest(URLRequest(url: URL(string: ("http://159.203.67.188/pdfs/" + (currentForm?.location)!))!))
-
+        pdfWebView.loadRequest(URLRequest(url: URL(string: "http://159.203.67.188/pdfs/\(currentForm!.location!)")!))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func fillOutButtonPressed(_ sender: AnyObject) {
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        destination = storyboard.instantiateViewController(withIdentifier: "EditPDFViewController") as! EditPDFViewController
-        destination.title = self.title
-        let backItem = UIBarButtonItem()
-        backItem.title = ""
-        destination.currentForm = self.currentForm
-        destination.navigationItem.backBarButtonItem = backItem
-        navigationController?.pushViewController(destination, animated: true)
     }
     
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "fillOutSegue" {
-            (segue.destination as! EditPDFViewController2).form = currentForm
+            // for scroll view
+            let destination = segue.destination as! EditPDFViewController
+            destination.title = self.title
+            let backItem = UIBarButtonItem()
+            backItem.title = ""
+            destination.currentForm = self.currentForm
+            destination.navigationItem.backBarButtonItem = backItem
+            
+            // for progress bar
+            // (segue.destination as! EditPDFViewController2).form = currentForm
         }
     }
 
